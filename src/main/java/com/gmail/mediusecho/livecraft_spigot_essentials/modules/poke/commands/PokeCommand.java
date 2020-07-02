4cf506013ce034f3fd95fdf264ce360040a32848
MediusEcho <mediusecho@gmail.com>
2020-07-02 15:53:25 -0700

    added PokeCommand

---
+++ src/main/java/com/gmail/mediusecho/livecraft_spigot_essentials/modules/poke/commands/PokeCommand.java
@@ -0,0 +1,59 @@
+package com.gmail.mediusecho.livecraft_spigot_essentials.modules.poke.commands;
+
+import com.gmail.mediusecho.fusion.annotations.Command;
+import com.gmail.mediusecho.fusion.annotations.Context;
+import com.gmail.mediusecho.fusion.annotations.Permission;
+import com.gmail.mediusecho.fusion.command.BukkitCommandSender;
+import com.gmail.mediusecho.fusion.commands.CommandListener;
+import com.gmail.mediusecho.livecraft_spigot_essentials.Lang;
+import com.gmail.mediusecho.livecraft_spigot_essentials.modules.poke.PokeModule;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.UUID;
+
+@Command(argument = "poke", contexts = "@player|ignore|unignore")
+public class PokeCommand extends CommandListener {
+
+    private final PokeModule pokeModule;
+
+    public PokeCommand (final PokeModule pokeModule)
+    {
+        this.pokeModule = pokeModule;
+    }
+
+    @Context(context = "@player")
+    @Permission(permission = "lce.command.modules.poke.player")
+    public void pokePlayer (@NotNull BukkitCommandSender sender)
+    {
+        List<String> arguments = sender.getFullArguments();
+        Player player = Bukkit.getPlayer(arguments.get(1));
+
+        if (player == null || !player.isOnline())
+        {
+            sender.sendMessage(Lang.PLAYER_ERROR.get().replace("{1}", arguments.get(1)));
+            return;
+        }
+
+        if (!sender.isPlayer())
+        {
+            pokeModule.poke(player);
+            sender.sendMessage(Lang.POKE_SUCCESS.get().replace("{1}", player.getDisplayName()));
+            return;
+        }
+
+        UUID id = sender.getPlayer().getUniqueId();
+        if (pokeModule.isIgnoringPlayer(player.getUniqueId(), sender.getPlayer())) {
+            return;
+        }
+
+        if (!pokeModule.canPoke(id)) {
+            return;
+        }
+
+        pokeModule.poke(player);
+        sender.sendMessage(Lang.POKE_SUCCESS.get().replace("{1}", player.getDisplayName()));
+    }
+}
