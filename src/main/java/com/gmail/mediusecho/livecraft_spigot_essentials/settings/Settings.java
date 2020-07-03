1a2372f9f07b565602150a6c3f6b64d448fc39dc
MediusEcho <mediusecho@gmail.com>
2020-07-02 18:12:05 -0700

    added emote settings

--- src/main/java/com/gmail/mediusecho/livecraft_spigot_essentials/settings/Settings.java
+++ src/main/java/com/gmail/mediusecho/livecraft_spigot_essentials/settings/Settings.java
@@ -14,4 +14,10 @@ public class Settings {
     public static final SettingsValue<Sound> POKE_SOUND = new SettingsValue<>("modules.poke.sound.id", Sound.ENTITY_CHICKEN_EGG);
     public static final SettingsValue<Float> POKE_SOUND_VOLUME = new SettingsValue<>("modules.poke.sound.volume", 1f);
     public static final SettingsValue<Float> POKE_SOUND_PITCH = new SettingsValue<>("modules.poke.sound.pitch", 1f);
+
+    public static final SettingsValue<Boolean> EMOTE_ENABLED = new SettingsValue<>("modules.emote.enabled", true);
+    public static final SettingsValue<Boolean> EMOTE_COMMANDS_ENABLED = new SettingsValue<>("modules.emote.commands.enabled", true);
+    public static final SettingsValue<Boolean> EMOTE_SIGNS_ENABLED = new SettingsValue<>("modules.emote.signs", true);
+    public static final SettingsValue<Boolean> EMOTE_ANVILS_ENABLED = new SettingsValue<>("modules.emote.anvils", true);
+    public static final SettingsPointer EMOTE_COMMAND_WHITELIST_POINTER = new SettingsPointer("modules.emote.commands.whitelisted-commands");
 }
